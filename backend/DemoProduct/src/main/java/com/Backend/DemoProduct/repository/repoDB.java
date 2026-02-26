c4d4a0fac65ce9864601c0ecb252cf96c4b79ba3
Balla Minar Sri Babu <minarsriballa@gmail.com>
2026-02-27 02:58:12 +0530

    Delete backend directory

--- backend/DemoProduct/src/main/java/com/Backend/DemoProduct/repository/repoDB.java
+++
@@ -1,9 +0,0 @@
-package com.Backend.DemoProduct.repository;
-
-import com.Backend.DemoProduct.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface repoDB extends JpaRepository<Product, Integer> {
-}
